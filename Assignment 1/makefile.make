8026062fcf8d03e94f17df32051e23afb2c2f064
thakkarv <thakkarv@bu.edu>
2017-09-10 22:40:40 -0400

    First try

--- Assignment 1/makefile.make
+++ Assignment 1/makefile.make
@@ -1,2 +1,4 @@
 CC = gcc
 
+ehco: my_echo.c
+	$(CC) my_echo.c -c -o my_echo
--- Assignment 1/my_echo.c
+++ Assignment 1/my_echo.c
@@ -2,7 +2,22 @@
 
 int main(int argc, char const * argv [])
 {
+	bool newline_at_end = true;
 
-	/* code */
+	if (argc > 1)
+	{
+		if (*argv[1] == "-n")
+			newline_at_end = false;
+	}
+	for (int i = counter; i < argc; ++i)
+	{
+		printf(*argv[i]);
+	}
+
+	if (newline_at_end)
+	{
+		printf("\n");
+	}
+
 	return 0;
 }
