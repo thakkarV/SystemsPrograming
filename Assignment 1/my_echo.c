cf79af64725ef440907f9f794d55d1690311d01e
thakkarv <thakkarv@bu.edu>
2017-09-10 22:54:24 -0400

    Additions for stupid mistakes

--- Assignment 1/my_echo.c
+++ Assignment 1/my_echo.c
@@ -1,13 +1,18 @@
-#include <>
+#include <stdio.h>
 
 int main(int argc, char const * argv [])
 {
 	bool newline_at_end = true;
+	int counter = 1;
 
 	if (argc > 1)
 	{
 		if (*argv[1] == "-n")
+		{
 			newline_at_end = false;
+			counter = 2;
+		}
+
 	}
 	for (int i = counter; i < argc; ++i)
 	{
