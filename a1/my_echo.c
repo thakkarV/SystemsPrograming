1e43f51858b440caa4c232964b7b22ca90d05cdd
thakkarv <thakkarv@bu.edu>
2017-09-12 22:28:07 -0400

    more elegant way of doing it

--- a1/my_echo.c
+++ a1/my_echo.c
@@ -14,22 +14,23 @@ int main(int argc, char const * argv [])
 		argc--;
 		if (strcmp(argv[0], newline_flag) == 0)
 		{
+			printf(argv[0]);
 			newline_at_end = 0;
-			if (argc > 2)
+			if (argc > 2 && !newline_at_end)
 			{
 				*argv++;
 				argc--;
-
-				while (argc > 0)
-				{
-					printf(*argv);
-					argv++;
-					argc--;
-
-					if (argc > 0)
-						printf(space);
-				}
 			}
+		}
+
+		while (argc > 0)
+		{
+			printf(*argv);
+			argv++;
+			argc--;
+
+			if (argc > 0)
+				printf(space);
 		}
 	}
 
