fb601b78c07bf538840e8c0a5a3b8475919b0db2
thakkarv <thakkarv@bu.edu>
2017-09-12 21:22:11 -0400

    fixes

--- a1/my_echo.c
+++ a1/my_echo.c
@@ -6,27 +6,20 @@ int main(int argc, char const * argv [])
 	int newline_at_end = 1;
 	int start = 1;
 	char * newline_flag = "-n";
-	char * space = " ";
 
-	if (argc > 1)
+	if (strcmp((argv[1]), newline_flag) == 0)
 	{
-		if (strcmp((argv[1]), newline_flag) == 0)
-		{
-			newline_at_end = 0;
-			start++;
-		}
-
-		if (argc > 2)
-		{
-			while (start < argc - 1)
-			{
-				printf(argv[start]);
-				printf(space);
-				start++;
-			}
-		}
-		printf(argv[argc - 1]);
+		newline_at_end = 0;
+		start++;
 	}
+
+	for (int i = start; start < argc - 1; ++i)
+	{
+		printf(argv[i]);
+		printf(" ");
+	}
+
+	printf(argv[argc - 1]);
 
 	if (newline_at_end)
 	{
