6ac2794cdd897b28a7e58a0a67d197a227af1afb
thakkarv <thakkarv@bu.edu>
2017-09-12 21:12:01 -0400

    rename for submission

---
+++ a1/assignment_1_tests.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import shutil
+from subprocess import Popen, PIPE
+
+def exec_prog(prog, exit_on_failure = True):
+	try:
+		process = Popen(prog, stdout=PIPE, stderr=PIPE)
+		(output, err) = process.communicate()
+		exit_code = process.wait()
+		if exit_code != 0 and exit_on_failure:
+			print str(prog), "returned ", exit_code
+			sys.exit(exit_code)
+		return output
+	except Exception as inst:
+		if exit_on_failure:
+			print "Failed to exec program", str(prog)
+			sys.exit(1)
+		else:
+			return ""
+
+
+def clean():
+	exec_prog(["make", "clean"])
+
+def build():
+	exec_prog(["make"])
+
+def test(args):
+	my_echo_output = exec_prog(["./my_echo"] + args, False);
+	echo_output = exec_prog(["echo"] + args);
+	print("TEST")
+	print("my echo : " + my_echo_output)
+	print("true echo: " + echo_output)
+	return my_echo_output == echo_output
+
+def tests():
+	success_list = []
+	test_cases = [
+		["-n", "fdas fdsa dfs"],
+		["-n"],
+		[],
+		["asdf"],
+		["-n", "asdf"],
+		["asdf dfsa fdas", "fasdf", "asdf"],
+		]
+
+	for test_case in test_cases:
+		result = test(test_case)
+		success_list.append(result)
+
+	return success_list
+
+def print_results(success_list):
+	i = 0
+	success_count = 0;
+	for s in success_list:
+		i += 1
+		if s:
+			success_count += 1
+		else:
+			print "Failed test", i
+
+	print "Grade:", success_count, "/", len(success_list)
+
+
+def main():
+	clean()
+	build()
+	print_results(tests())
+
+if __name__ == "__main__":
+		main()
---
+++ a1/my_echo.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const * argv [])
+{
+	int newline_at_end = 1;
+	int start = 1;
+	char * newline_flag = "-n";
+	char * space = " ";
+
+	if (argc > 1)
+	{
+		if (strcmp((argv[1]), newline_flag) == 0)
+		{
+			newline_at_end = 0;
+			start++;
+		}
+
+		if (argc > 2)
+		{
+			while (start < argc - 1)
+			{
+				printf(argv[start]);
+				printf(space);
+				start++;
+			}
+		}
+		printf(argv[argc - 1]);
+	}
+
+	if (newline_at_end)
+	{
+		printf("\n");
+	}
+
+	return 0;
+}
