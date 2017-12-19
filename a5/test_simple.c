b23d8b835202f5574d0cc295841241bad3e62763
thakkarv <thakkarv@bu.edu>
2017-12-19 16:30:46 -0500

    better test case

--- a5/test_simple.c
+++ a5/test_simple.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const * argv [])
 {
+	int a = 5;
 	printf("This is a very simple test program.\n");
-
+	int b = 7;
 	printf("It only prints out 2+2=%d\n", 2+2);
-
-	return 1;
+	int c = a + b / 2;
+	printf("(a + b) / 2 = %d", c);
+	return 0;
 }
