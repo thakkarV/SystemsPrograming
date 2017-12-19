7bd48f0dcbd75c86ce68b60bb43b6d2fdf17baf7
thakkarv <thakkarv@bu.edu>
2017-12-19 16:56:10 -0500

    fixes:

--- a5/parser.c
+++ a5/parser.c
@@ -41,7 +41,7 @@ void parse(char ** tokens, int num_args)
 		(strcmp(tokens[0], CMD_QUIT_SHORT) == 0))
 	{
 		if (is_running)
-			if (getYN("A debugging session is active. Quit anyway?"));
+			if (getYN("A debugging session is active. Quit anyway?"))
 				do_quit();
 		else
 			exit(0);
